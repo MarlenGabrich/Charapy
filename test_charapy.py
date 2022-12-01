6927e4f03297c3d7d839127a9f22ee6fac15a8c8
marlengabrich <marlen.gabrich@mi.unc.edu.ar>
2022-12-01 19:56:34 -0300

    tests

--- test_charapy.py
+++ test_charapy.py
@@ -3,7 +3,7 @@ from test import support
 import charapy as char
 import numpy as np
 
-class MyTestCase1(unittest.TestCase):
+class MyTestCaseFT(unittest.TestCase):
 
     def setUp(self):
         #borrar si no lo necesite
@@ -15,7 +15,6 @@ class MyTestCase1(unittest.TestCase):
 
     def test_fitAB(self):
         foo_fit = char.Foo_fit()
-
         x = np.array([1,2])
         y = np.array([2,3])
         A = 2.4663
@@ -23,6 +22,19 @@ class MyTestCase1(unittest.TestCase):
         
         self.assertEqual(foo_fit.fit_AB(x,y)[0][0].round(4),A)
         self.assertEqual(foo_fit.fit_AB(x,y)[0][1].round(4),B)
+        self.assertEqual(foo_fit.fit_LM(y,x)[0][0].round(4),A)
+        self.assertEqual(foo_fit.fit_LM(y,x)[0][1].round(4),B)
+
+class MyTestCaseDP(unittest.TestCase):
+
+    def test_carbonnumber(self):
+        dist_pedersen = char.Distribution_pedersen()
+        x = 2
+        c1 = 1
+        c2 = 2
+        cod = 2.6931
+        self.assertEqual(dist_pedersen.carbon_number(x,c1,c2).round(4),cod)
+        self.assertEqual(dist_pedersen.p_density(x,c1,c2).round(4),cod)
 
 if __name__ == '__test__':
     unittest.main()
